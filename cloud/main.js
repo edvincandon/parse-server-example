346cb94c88076b5f5a02deacd92d5bdec2fa9dad
Edvin CANDON <edvincandon@gmail.com>
2016-12-22 14:20:12 +0100

    hg

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,30 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define('addFriend', function(req, res) {
+  // check if request exists on other end -> toUser = req.fromUser
+  // if exists set it to status APPROVED
+  var User = Parse.Object.extend('User');
+  var Request = Parse.Object.extend('Request');
+
+  var originUser = req.user;
+  var targetUserID = req.params.toUser;
+  var targetUser = new User();
+  targerUser.id = targetUserID;
+
+
+  var friendRequest = new Request();
+  friendRequest.save({
+    fromUser: originUser,
+    toUser: targetUser
+    }, {
+      success: function(result) {
+        res.success(result);
+      },
+      error: function(result, error) {
+        res.error(error);
+      }
+  });
+
+});
---
+++ dashboard.command
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "-------STARTING XQUIZIT DASHBOARD-------"
+parse-dashboard --appId XQUIZIT_APP_VINED --masterKey XQUIZIT_12345678910 --serverURL "https://xquizit.herokuapp.com/parse"
