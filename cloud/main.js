0ebc3810715a67ac101a64efd0b988f6eebe95de
Edvin CANDON <edvincandon@gmail.com>
2016-12-23 11:05:13 +0100

    hg

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,3 @@
 var friends = require("./friends.js");
 
-Parse.Cloud.define('addFriend', friends.addFriend(req, res));
+Parse.Cloud.define('addFriend', function(req, res){ friends.addFriend(req, res) });
