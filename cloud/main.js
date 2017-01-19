2ac7648c365927bc7d9c45faf0ba1b2f26eb4341
Edvin CANDON <edvincandon@gmail.com>
2017-01-19 10:53:45 +0100

    update

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,4 @@ var friends = require("./friends.js");
 
 Parse.Cloud.define('addFriend', function(req, res){ friends.addFriend(req, res) });
 Parse.Cloud.define('getAllRequests', function(req, res){ friends.getAllRequests(req, res) });
+Parse.Cloud.define('acceptFriendRequest', function(req, res){ friends.acceptFriendRequest(req, res) });
